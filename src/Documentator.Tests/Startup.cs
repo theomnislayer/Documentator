d011462122fd84ced23099ecb55574beec93443e
Glynn Bacanto <glynnbacanto@gmail.com>
2025-04-09 23:53:39 +0800

    Added dotEnv and Startup

---
+++ src/Documentator.Tests/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Documentator.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Documentator.Tests;
+
+[TestClass]
+public class Startup
+{
+    public static ServiceProvider ServiceProvider;
+
+    [AssemblyInitialize()]
+    public static void AssemblyInit(TestContext context)
+    {
+        ServiceProvider = ConfigureServices().BuildServiceProvider();
+    }
+
+    [AssemblyCleanup()]
+    public static async Task AssemblyCleanup()
+    {
+        await ServiceProvider.DisposeAsync();
+    }
+
+    public static IServiceCollection ConfigureServices()
+    {
+        DotEnv.Load();
+        return new ServiceCollection()
+            .AddClients();
+    }
+}
