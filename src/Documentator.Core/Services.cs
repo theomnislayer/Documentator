0b00f57d14e6215a4c3fab0c5e7293f180c3b3cc
Glynn Bacanto <glynnbacanto@gmail.com>
2025-04-09 23:06:06 +0800

    Add project files.

---
+++ src/Documentator.Core/Services.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Documentator.Core;
+
+public static class Services
+{
+	public static IServiceCollection AddClients(this IServiceCollection services) => services
+		.AddSingleton<HttpClientFactory>();
+}
